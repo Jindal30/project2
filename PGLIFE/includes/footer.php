a82bd01238f64d62b30cd62513990ef25d1ad644
Jindal30 <141744772+Jindal30@users.noreply.github.com>
2024-05-27 12:59:26 +0530

    Update footer.php

--- PGLIFE/includes/footer.php
+++ PGLIFE/includes/footer.php
@@ -20,6 +20,6 @@
                 <a href="<?php echo $property_list_path.'?city=Hyderabad'; ?>">PG in Hyderabad</a>
             </div>
         </div>
-        <div class="footer-copyright">© 2022 Copyright PG Life </div>
+       
     </div>
 </div>
